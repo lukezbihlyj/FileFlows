7cc4a046c74dcd1283a537f63c9363f477819c5e
John Andrews <john@reven.co.nz>
2024-10-03 09:30:03 +1300

    auto tests

--- AutoTests/AutoTests/TestFiles.cs
+++ AutoTests/AutoTests/TestFiles.cs
@@ -3,5 +3,6 @@ namespace FileFlowsTests;
 public class TestFiles
 {
     public const string TestPath = "/app/tests-results/recordings";
-    public const string TestVideo1 = TestPath + "/FileFlowsTests.Tests.InitialTests.InitialConfiguration/FileFlowsTests.Tests.InitialTests.InitialConfiguration.webm";
+    public const string TestVideo1ShortName = "FileFlowsTests.Tests.InitialTests.IT_01_InitialConfiguration.webm";
+    public const string TestVideo1 = TestPath + "/FileFlowsTests.Tests.InitialTests.InitialConfiguration/" + TestVideo1ShortName;
 }
