e778828fce761a5e3b880bc83e5dbab0ec725e9f
John Andrews <john@reven.co.nz>
2024-11-14 14:12:00 +1300

    dockermods limit

--- AutoTests/AutoTests/TestFiles.cs
+++ AutoTests/AutoTests/TestFiles.cs
@@ -3,6 +3,6 @@ namespace FileFlowsTests;
 public class TestFiles
 {
     public const string TestPath = "/app/test-results/recordings";
-    public const string TestVideo1ShortName = "FileFlowsTests.Tests.InitialTests.IT_01_InitialConfiguration.webm";
-    public const string TestVideo1 = TestPath + "/FileFlowsTests.Tests.InitialTests.IT_01_InitialConfiguration/" + TestVideo1ShortName;
+    public const string TestVideo1ShortName = "FileFlowsTests.Tests.InitialTests.IT_02_InitialConfiguration.webm";
+    public const string TestVideo1 = TestPath + "/FileFlowsTests.Tests.InitialTests.IT_02_InitialConfiguration/" + TestVideo1ShortName;
 }
